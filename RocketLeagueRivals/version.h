e87a73131d9eea2c075d7dfd4c8d287f596169a4
gitthedom <65032991+gitthedom@users.noreply.github.com>
2024-06-26 15:59:53 -0400

    percentage update
    - fixed some line height weirdness from adding demos
    
    - converted to using percentages in showing how many games won against or with a player

--- RocketLeagueRivals/version.h
+++ RocketLeagueRivals/version.h
@@ -2,7 +2,7 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 1
-#define VERSION_BUILD 3
+#define VERSION_BUILD 9
 
 #define stringify(a) stringify_(a)
 #define stringify_(a) #a
